cb3a614abae3f236e04274c20f6d5733d67aec2b
Nurudeen <oseninurudeenafolabi@yahoo.com>
2024-01-13 21:19:23 +0000

    final commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# A program to find the area of a triangle
+
+height = float(input("Enter your height ? "))
+base = float(input("What is your base ? "))
+
+# Calculate the area 1 / 2 * base * height
+
+area = (base * height) / 2
+
+# To display result
+
+print(f"The Area of the triangle is:  {area}")
